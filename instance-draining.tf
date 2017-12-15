e91cc8f78fc0bb3ccb406d6bc66b5f93898e22a3
Miguel Ferreira <miguelferreira@me.com>
2017-12-15 15:56:33 +0100

    Get lambda function name from resource to make inplicit dependency

--- instance-draining.tf
+++ instance-draining.tf
@@ -165,7 +165,7 @@ resource "aws_sns_topic_subscription" "autoscaling_lambda" {
 resource "aws_lambda_permission" "autoscaling_lambda" {
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
-  function_name = "${var.instance_drainer_function_name}"
+  function_name = "${aws_lambda_function.instance_draining.function_name}"
   principal     = "sns.amazonaws.com"
   source_arn    = "${aws_sns_topic.autoscaling.arn}"
 }
